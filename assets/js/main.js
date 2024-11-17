636f51daf63ff019df42fbf6259121210c428572
Kahina Ouali <kahinaouali.pro@gmail.com>
2024-11-17 21:32:40 +0100

    New version

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,14 @@ document.addEventListener("scroll", () => {
     }
 
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".nav-link");
+  
+    menuToggle.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+      menuToggle.classList.toggle("active");
+    });
+  });
+  
